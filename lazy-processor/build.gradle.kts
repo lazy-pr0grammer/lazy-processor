b9311048d8e96eaf41f6e48a55b3d21d2dd5e739
lazy-pr0grammer <lazyprogrammer21@gmail.com>
2023-12-02 11:54:22 -0500

    feat: removed duplicate folder

--- lazy-processor/build.gradle.kts
+++ lazy-processor/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") version "1.9.0"
     id("com.google.devtools.ksp") version "1.9.0-1.0.13"
 }
 
 dependencies {
     //noinspection GradleDependency
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
+    //noinspection GradleDependency
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.0-1.0.13")
+
 }
