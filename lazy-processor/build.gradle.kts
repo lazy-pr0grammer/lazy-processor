ba320f295168135be9862de67aa944ed33ea5cea
lazy-pr0grammer <lazyprogrammer21@gmail.com>
2023-12-03 04:43:27 +0600

    fix: gradle issues

--- lazy-processor/build.gradle.kts
+++ lazy-processor/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     id("com.google.devtools.ksp") version "1.9.0-1.0.13"
+    id("maven-publish")
+}
+
+java {
+   withSourcesJar()
 }
 
 dependencies {
@@ -8,5 +13,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
     //noinspection GradleDependency
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.0-1.0.13")
+}
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
 }
